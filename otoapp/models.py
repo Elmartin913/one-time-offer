e286ef9f8e10b27b5eb04b688a38baaf33e61c7d
Marcin Jablonowski <marcin.jab@wp.pl>
2018-03-17 21:14:58 +0100

    Model ShortURL

--- otoapp/models.py
+++ otoapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class ShortURL(models.Model):
+    url = models.CharField(max_length=256, validators=[validate_url, validate_dot_com])
+    shortcode = models.CharField(max_length=SHORTCODE_MAX, unique=True, blank=True, null=True)
+    update = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    active = models.BooleanField(default=True)
