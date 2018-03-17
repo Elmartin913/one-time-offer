c2ed1231e19cde1ad17c173906b030c733b114b9
Marcin Jablonowski <marcin.jab@wp.pl>
2018-03-17 21:49:03 +0100

    Home View

--- oto/urls.py
+++ oto/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from otoapp.views import HomeView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('panel/', HomeView.as_view(), name='panel')
 ]
--- otoapp/views.py
+++ otoapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
+from .forms import SubmitUrlForm
 # Create your views here.
+
+class HomeView(View):
+    def get(self, request, *args, **kwargs):
+        the_form = SubmitUrlForm()
+        context = {
+            'form': the_form,
+            'title': 'Submit url: ',
+        }
+        return render(request, 'home.html', context)
