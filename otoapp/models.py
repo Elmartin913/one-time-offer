b83b1df7e545032739af77e469d5ca5b9e88dbc4
Marcin Jablonowski <marcin.jab@wp.pl>
2018-03-17 21:26:09 +0100

    Admin - register ShortURL model

--- otoapp/models.py
+++ otoapp/models.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 
+SHORTCODE_MAX = getattr(settings, 'SHORTCODE_MAX', 15)
+
 class ShortURL(models.Model):
-    url = models.CharField(max_length=256, validators=[validate_url, validate_dot_com])
+    url = models.CharField(max_length=256)
     shortcode = models.CharField(max_length=SHORTCODE_MAX, unique=True, blank=True, null=True)
     update = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
